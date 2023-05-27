b5d8ac27369a722f641507cc72bd2250f2ed6886
Sara Gunda <foodpiles@Saras-MacBook-Air.local>
2023-05-27 14:39:58 -0400

    route edits

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,43 +1,41 @@
-const Comic = require('../models/comic-model');
-const { response } = require('express');
+const Comic = require("../models/comic-model");
+// const { response } = require('express');
 
 module.exports = {
-
   admin: (request, response) => {
     if (request.isAuthenticated()) {
-      Comic.find({}).then(allComics => {
-        response.render('pages/admin',{
-          data: allComics
-        })
-      })
+      Comic.find({}).then((allComics) => {
+        response.render("pages/admin", {
+          data: allComics,
+        });
+      });
     } else {
-      response.redirect('/login')
+      response.redirect("/login");
     }
   },
 
   admin_update: (request, response) => {
-    if(request.isAuthenticated()){
+    if (request.isAuthenticated()) {
       const { _id } = request.params;
-      Comic.findOne({_id: _id}).then((foundComic, error) => {
-        if(error) {
+      Comic.findOne({ _id: _id }).then((foundComic, error) => {
+        if (error) {
           return error;
-        }  else {
-          response.render('pages/update', {
-          data: foundComic,
-        });
-      }
-    });    
+        } else {
+          response.render("pages/update", {
+            data: foundComic,
+          });
+        }
+      });
     } else {
-      response.redirect('/login')
+      response.redirect("/login");
     }
   },
 
   admin_create: (request, response) => {
-    if(request.isAuthenticated()){
-      response.render('pages/create', {
-    });
+    if (request.isAuthenticated()) {
+      response.render("pages/create");
     } else {
-      response.redirect('/login')
+      response.redirect("/login");
     }
-  }
-}
+  },
+};
