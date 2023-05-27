09b3dcd62edfa773ed474f61d1c683857efeb55e
Sara Gunda <foodpiles@Saras-MacBook-Air.local>
2023-05-27 10:59:30 -0400

    connection.js code change

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,13 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URL).catch(err => {
-  if(err) {
-    console.log("There was an error.");  
-  }
-  console.log ("Connection successful!")
+  // if(err) {
+  //   console.log("There was an error.");  
+  // }
+  // console.log ("Connection successful!")
+  main().catch(err => console.log(err));
+  async function main() {
+  await mongoose.connect(process.env.DB_URL)};  
 });
 
 // mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
