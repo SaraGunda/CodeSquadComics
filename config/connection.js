493414747c37679d5a91571eaf1585737e8e86a1
Sara Gunda <foodpiles@Saras-MacBook-Air.local>
2023-05-27 11:16:28 -0400

    minor cat change, code cleanup

--- config/connection.js
+++ config/connection.js
@@ -6,21 +6,9 @@
 
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.DB_URL).catch(err => {
-  // if(err) {
-  //   console.log("There was an error.");  
-  // }
-  // console.log ("Connection successful!")
+
 main().catch(err => console.log(err));
   async function main() {
   await mongoose.connect(process.env.DB_URL)};  
 
-// mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-//   if(!err) {
-//     console.log("You have successfully connected to the MongoDB server! Hooray!");  
-//   }
-//   else {
-//       console.log("ERROR- there is a connectivity issue with MongoDB. Please review.");
-//       console.log(err)
-//   }
-// });
+
